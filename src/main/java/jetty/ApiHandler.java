d3018d021c4241b93770ae687ed6d7d6433783ac
Kevin Tang <kevin_m_tang@yahoo.com>
2017-10-20 21:27:46 -0700

    commit

--- src/main/java/jetty/ApiHandler.java
+++ src/main/java/jetty/ApiHandler.java
@@ -82,8 +82,8 @@ public class ApiHandler extends AbstractHandler
 			stmt = c.createStatement();
 			
 			String sql = "TRUNCATE TABLE \"menu_items\";";
-			stmt.execute(sql);
-			
+			//stmt.executeUpdate(sql);
+			/*
 			sql = "CREATE TABLE \"menu_items\" ("
 					+ "name varchar(255),"
 					+ "restaurant varchar(255),"
@@ -107,7 +107,9 @@ public class ApiHandler extends AbstractHandler
 					+ "saturday boolean,"
 					+ "sunday boolean"
 					+ ");";
+					
 			stmt.executeUpdate(sql);
+			*/
 			
 			sql = "INSERT INTO \"menu_items\" (name, restaurant, cost, calories, fat, trans_fat, cholesterol, sodium, carbohydrates, fiber, sugars, protein, allergens, monday, tuesday, wednesday, thursday, friday, saturday, sunday) "
 		            + "VALUES ('name', 'restaurant', 32, 40, 100.0, 90.0, 1531.3, 103.2, 1031.1, 123.2, 123.1, 123.1, '{\"soy\", \"milk\"}', true, true, true, true, true, true, true);";
